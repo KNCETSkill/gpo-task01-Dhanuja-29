1391f4595c99e238aee9c4361a13e213fd64c78c
Dhanuja-29 <thanujanagarajan29@gmail.com>
2026-01-19 16:06:59 +0530

    Update BrewSizeAdvisor.java

--- src/BrewSizeAdvisor.java
+++ src/BrewSizeAdvisor.java
@@ -9,7 +9,7 @@ public class BrewSizeAdvisor {
 
         int volume = sc.nextInt();
         
-        if((volume>0) &&(volume<200)){
+        if((volume>=0) &&(volume<200)){
             System.out.println("Small");
         }
         else if((volume>=200)&&(volume<350)){
